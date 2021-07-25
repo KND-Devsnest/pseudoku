6731ceb63af5b378542e0bbc2d229be90812d5ba
kaushikvrudhula <84515709+kaushikvrudhula@users.noreply.github.com>
2021-07-25 21:30:22 +0530

    added Howtoplay

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
-
+import Howtoplay from "./components/Howtoplay";
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Howtoplay />
       <Footer />
     </div>
   );
