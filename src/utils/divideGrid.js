432738ef59cb10485f5b96cb65acf3e6cc2a1e9e
aguyran <59661860+aguyran@users.noreply.github.com>
2021-07-25 23:13:32 +0530

    fix colours

--- src/utils/divideGrid.js
+++ src/utils/divideGrid.js
@@ -6,22 +6,22 @@ export const divideGrid = (i, j) => {
     (i === 5 && j === 2)
   ) {
     return {
-      borderBottom: "0.25rem solid #053742",
-      borderRight: "0.25rem solid #053742",
+      borderBottom: "0.25rem solid var(--dark)",
+      borderRight: "0.25rem solid var(--dark)",
     };
   }
   switch (i) {
     case 2:
     case 5:
       return {
-        borderBottom: "0.25rem solid #053742",
+        borderBottom: "0.25rem solid var(--dark)",
       };
   }
   switch (j) {
     case 2:
     case 5:
       return {
-        borderRight: "0.25rem solid #053742",
+        borderRight: "0.25rem solid var(--dark)",
       };
   }
 };
