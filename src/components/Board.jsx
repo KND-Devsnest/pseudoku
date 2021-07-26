69be852e00aa5436ee7dda8b37fc04e53139fbdc
Swanand Buva <swanandev@gmail.com>
2021-07-26 12:56:57 +0530

    Added Little Fix

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -39,6 +39,7 @@ const Board = ({ randomElement }) => {
       //console.log(randomElement.puzzle);
       setPuzzle(JSON.parse(JSON.stringify(randomElement.puzzle)));
       setSelected(false);
+      setDone([]);
     };
     dispatch({ type: "SET_RESET_PUZZLE", value: resetPuzlle });
     dispatch({ type: "SET_UNDO_PUZZLE", value: undoPuzzle });
